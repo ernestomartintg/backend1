32116e996864b473c580d4cfc3bf0bc0bbd4acb1
Ernesto Torres <ernestomartintg@gmail.com>
2024-11-13 11:12:56 -0500

    User Service

---
+++ src/main/java/pe/joedayz/backend/service/UserService.java
@@ -0,0 +1,26 @@
+package pe.joedayz.backend.service;
+
+import org.springframework.stereotype.Service;
+import pe.joedayz.backend.model.User;
+import pe.joedayz.backend.repo.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<User> getUsers(){
+        return repo.findAll();
+    }
+
+    public User getUser(Long id){
+        return repo.findById(id).orElseThrow(()->
+                new UserNotFoundException("User by id "+id+"was not found"));
+    }
+}
